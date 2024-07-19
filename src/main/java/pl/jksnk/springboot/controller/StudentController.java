f6253bbbb25dd4d2c8be22d24d67e0ae69aaf9ca
Jakub <jakub.kucaba@gmail.com>
2024-07-19 12:43:16 +0200

    Returning a List as a JSON to the client

--- src/main/java/pl/jksnk/springboot/controller/StudentController.java
+++ src/main/java/pl/jksnk/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.jksnk.springboot.bean.Student;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -17,5 +20,17 @@ public class StudentController {
                 "Głowacki"
         );
         return student;
+    }
+
+    //http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student(1, "Jakub", "Głowacki"));
+        students.add(new Student(2, "Łukasz", "Słowacki"));
+        students.add(new Student(3, "Patryk", "Prądnicki"));
+        students.add(new Student(4, "Mikuś", "Wrocławski"));
+
+        return students;
     }
 }
